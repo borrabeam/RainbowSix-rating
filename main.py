9ac46ca376e368556d43f770c22f9c8ac061a174
borrabeam <bannakorn.s@ku.th>
2022-04-28 14:07:02 +0700

    testing add character

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from rainbow import RainbowSix
+from models.character_model import Character
+from models.rating_model import UserRating
 
 rainbowsix_db = RainbowSix.get_instance()
 
@@ -6,3 +8,7 @@ character_dao = rainbowsix_db.get_character_dao()
 rating_dao = rainbowsix_db.get_rating_dao()
 
 print(character_dao.get_character())
+
+# new_character = Character(id=51, character_name="Beam",side="ATTACKER",talent="Fly")
+# character_dao.add_character(new_character)
+# print(character_dao.get_character_id("51"))
